8d47b2cf6e78dca9231f54556631200436a04a69
Andrew Mingst <amingst97@gmail.com>
2019-11-17 18:44:52 -0500

    refactored load_data

--- main.py
+++ main.py
@@ -6,19 +6,34 @@ from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 
 
+def remove_zeros(data_in):
+    data_out = data_in
+
+    for x in range(0, data_out.shape[0]):
+        for y in range(0, data_out.shape[1]):
+            if (data_out[x][y] == 0):
+                data_out[x][y] = data_out[x-1][y]
+
+    return data_out
+
+
+def to_array3D(data_in, num_days):
+    data_out = []
+
+    for index in range(len(data_in) - num_days):
+        data_out.append(data_in[index: index + num_days])
+
+    return data_out
+
+
 def load_data(filename, num_days):
     raw_data = pd.read_csv(filename, dtype=float).values
 
-    for x in range(0, raw_data.shape[0]):
-        for y in range(0, raw_data.shape[1]):
-            if (raw_data[x][y] == 0):
-                raw_data[x][y] = raw_data[x-1][y]
+    raw_data = remove_zeros(raw_data)
 
     data = raw_data.tolist()
 
-    data_3D = []
-    for index in range(len(data) - num_days):
-        data_3D.append(data[index: index + num_days])
+    data_3D = to_array3D(data, num_days)
 
     print(data_3D)
 
