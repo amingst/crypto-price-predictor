68c73d6f692129545309519ab3ac954691be9010
Andrew Mingst <amingst97@gmail.com>
2019-11-17 18:00:33 -0500

    added imports and bitcoin data

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Bidirectional
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
