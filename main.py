302351602d41ca1d3fbdcbc664d12dd4a0c897ff
Andrew Mingst <amingst97@gmail.com>
2019-11-17 20:44:27 -0500

    added documentation for to_array3D

--- main.py
+++ main.py
@@ -34,6 +34,21 @@ def remove_zeros(data_in):
 
 
 def to_array3D(data_in, num_days):
+    """ Takes an input array and the number of days and converts to a 3D array.
+
+    Parameters
+    ----------
+    data_in: list, required
+            The input data in a 2D array.
+    num_days: int, required
+            The number of days to take from the input data.
+    
+    Returns
+    -------
+    data_out: list
+            An output list that is 3 Dimensional.
+
+    """
     data_out = []
 
     for index in range(len(data_in) - num_days):
