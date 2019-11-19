6a61e133f8d5ecb0833f839f160d64c14a662dcf
Andrew Mingst <amingst97@gmail.com>
2019-11-19 15:18:20 -0500

    added function that initializes the model

--- src/main.py
+++ src/main.py
@@ -5,9 +5,34 @@ from keras.layers import Bidirectional
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 
+
+def init_model(forecast, dropout, activation, loss, optimizer, x_train):
+    model = Sequential()
+
+    model.add(Bidirectional(LSTM(forecast, return_sequences=True),
+                            input_shape=(forecast, x_train.shape[-1]),))
+    model.add(Dropout(dropout))
+
+    model.add(Bidirectional(LSTM((forecast*2), return_sequences=True)))
+    model.add(Dropout(dropout))
+
+    model.add(Bidirectional(LSTM(forecast, return_sequences=False)))
+
+    model.add(Dense(units=1))
+
+    model.add(Activation(activation))
+
+    model.compile(loss=loss, optimizer=optimizer)
+
+    return model
+
+
 def main():
-    x_train, y_train, x_test, y_test, prev_y, unnormalized, w_size = load_data(
-    "../data/bitcoin_historical.csv", 50)
+    x_train, y_train, x_test, y_test, prev_y, unnormalized, forecast = load_data(
+        "../data/bitcoin_historical.csv", 50)
+
+    model = init_model(forecast, 0.2, 'linear', 'mse', 'adam', x_train)s
+
 
 if __name__ == "__main__":
     main()
